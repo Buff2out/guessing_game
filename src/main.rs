dd4ebb4cc1e20a413abc9d480873699f02cd350c
Buff2out <slava.070299@gmail.com>
2024-10-29 00:50:27 +0300

    some works and research with book

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use rand::Rng;
 use std::io;
 fn main() {
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    println!("{secret_num}");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to readline");
-    println!("{guess}");
+    print!("{guess}");
 }
