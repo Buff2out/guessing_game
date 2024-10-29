dbaa397d7815191de9a9e8574cb0a39eb15163a3
Buff2out <slava.070299@gmail.com>
2024-10-29 15:43:04 +0300

    added compare

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
-    let secret_num = rand::thread_rng().gen_range(1..=100);
-    println!("{secret_num}");
+    let secret_num: u32 = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to readline");
-    print!("{guess}");
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("invalid type casting from string to u32");
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("guess less than secret_num"),
+        Ordering::Greater => println!("guess greater than secret_num"),
+        Ordering::Equal => println!("GOTCHA"),
+    }
 }
