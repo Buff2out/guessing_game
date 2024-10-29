769240914bb404ab93349460aa2ae94125a1948a
Buff2out <slava.070299@gmail.com>
2024-10-29 15:54:28 +0300

    added loop

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@ use std::cmp::Ordering;
 use std::io;
 fn main() {
     let secret_num: u32 = rand::thread_rng().gen_range(1..=100);
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to readline");
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("invalid type casting from string to u32");
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("guess less than secret_num"),
-        Ordering::Greater => println!("guess greater than secret_num"),
-        Ordering::Equal => println!("GOTCHA"),
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to readline");
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("invalid type casting from string to u32");
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("guess less than secret_num"),
+            Ordering::Greater => println!("guess greater than secret_num"),
+            Ordering::Equal => {
+                println!("GOTCHA");
+                break;
+            }
+        }
     }
 }
